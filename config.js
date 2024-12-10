27b55653e20f32bc94510889afb650b3f54da7e0
lux <rob.kunkle@gmail.com>
2024-12-09 23:42:53 -0800

    adding dev/production config

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+  development: {
+    supabaseUrl: "https://dasvvxptyafaiwkmmmqz.supabase.co",
+    supabaseKey: "...",
+    redirectUrl: "https://localhost:3000",
+  },
+  production: {
+    supabaseUrl: "https://dasvvxptyafaiwkmmmqz.supabase.co",
+    supabaseKey: "...",
+    redirectUrl: "https://emojikey.io",
+  },
+};
