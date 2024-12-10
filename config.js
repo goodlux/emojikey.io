2d3a013957c8651d7a477e5d0a870c6827e00a99
lux <rob.kunkle@gmail.com>
2024-12-09 23:56:33 -0800

    debugging login

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   development: {
     supabaseUrl: "https://dasvvxptyafaiwkmmmqz.supabase.co",
     supabaseKey: "...",
-    redirectUrl: "https://localhost:3000",
+    redirectUrl: "https://127.0.0.1:3000",
   },
   production: {
     supabaseUrl: "https://dasvvxptyafaiwkmmmqz.supabase.co",
